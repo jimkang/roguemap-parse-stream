d02e50ba15e06a51b3de56371d6cca8aec474a1d
Jim Kang <jimkang@gmail.com>
2014-05-07 09:08:57 -0400

    Added text for cell.key to each cell rendition. Updated group transform to not use scale, as that affected the text as well as the rect.

--- example/renderer.js
+++ example/renderer.js
@@ -1,10 +1,10 @@
 function createRenderer(opts) {
   if (!opts.spaceFactor) {
-    opts.spaceFactor = 20;
+    opts.spaceFactor = 50;
   }
 
   var accessors = createAccessors({
-    simpleAccessors: ['id']
+    simpleAccessors: ['id', 'key']
   });
 
   var tileRoot = d3.select(opts.rootSelector);
@@ -12,20 +12,27 @@ function createRenderer(opts) {
   function renderCells(cells) {
     var cellRenditions = tileRoot.selectAll('.cell').data(cells, accessors.id);
     var newRenditions = cellRenditions.enter().append('g');
-    newRenditions.classed('cell');
+    newRenditions.classed('cell', true);
+    
     newRenditions.append('rect');
-    newRenditions.append('text');
+    newRenditions.append('text').attr({
+      x: '1em',
+      y: '1em',
+      dy: '0.35em',
+      'text-anchor': 'middle'
+    });
 
     cellRenditions.selectAll('rect').attr({
-      width: 1,
-      height: 1,
+      width: opts.spaceFactor,
+      height: opts.spaceFactor,
       fill: 'blue'
     });
     cellRenditions.attr('transform', function composeTransform(d) {
       return 'translate(' + d.coords[0] * opts.spaceFactor + ',' + 
-        d.coords[1] * opts.spaceFactor + 
-        ') scale(' + opts.spaceFactor + ')';
+        d.coords[1] * opts.spaceFactor + ')';
     });
+
+    cellRenditions.selectAll('text').text(accessors.key);
   }
 
   return {
