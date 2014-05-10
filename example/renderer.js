0f02d734d1af9b8ad598fdcba49f27000f047e31
Jim Kang <jimkang@gmail.com>
2014-05-09 20:32:18 -0400

    Dropped setting id function on selections in renderer since we never go back and update them anyway.

--- example/renderer.js
+++ example/renderer.js
@@ -10,12 +10,16 @@ function createRenderer(opts) {
   var tileRoot = d3.select(opts.rootSelector);
 
   function renderNewCells(cells) {
-    var cellRenditions = tileRoot.selectAll('.new').data(cells, accessors.id);
-
+    var cellRenditions = tileRoot.selectAll('.new').data(cells);    
     var newRenditions = cellRenditions.enter().append('g');
     newRenditions.classed('cell', true);
+
+    newRenditions.append('rect').attr({
+      width: opts.spaceFactor,
+      height: opts.spaceFactor,
+      fill: 'blue'
+    });
     
-    newRenditions.append('rect');
     newRenditions.append('text').attr({
       x: '1em',
       y: '1em',
@@ -23,11 +27,6 @@ function createRenderer(opts) {
       'text-anchor': 'middle'
     });
 
-    newRenditions.selectAll('rect').attr({
-      width: opts.spaceFactor,
-      height: opts.spaceFactor,
-      fill: 'blue'
-    });
     newRenditions.attr('transform', function composeTransform(d) {
       return 'translate(' + d.coords[0] * opts.spaceFactor + ',' + 
         d.coords[1] * opts.spaceFactor + ')';
